f024c179a945e74878a4058407022824b3e4312c
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-12-18 18:12:30 +0200

    Fix prettier problem

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,33 @@
+/* eslint-disable prettier/prettier */
 import './Person.css';
 
 export const Person = ({ person }) => {
   const { name, age, isMarried, partnerName, sex } = person;
-
+  const partnerRole = sex === 'm' ? 'wife' : 'husband';
+  
   return (
     <section className="Person">
       <h2 className="Person__name">{`My name is ${name}`}</h2>
-      {age === undefined ? null : (
-        <p className="Person__age">{`I am ${age}`}</p>
-      )}
-      {isMarried === true ? (
-        <p className="Person__partner">{`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}</p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+    {
+      age === undefined
+        ? null
+        : (
+        <p className="Person__age">
+          {`I am ${age}`}
+        </p>
+        )
+    }
+      {isMarried === true
+        ? (
+          <p className="Person__partner">
+            {`${partnerName} is my ${partnerRole}`}
+          </p>
+        )
+        : (
+          <p className="Person__partner">
+            I am not married
+          </p>
+        )}
     </section>
   );
 };
